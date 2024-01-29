3078f7e8acc3d823842806c975b4d67c45ee15a4
yohan-work <149753055+yohan-work@users.noreply.github.com>
2024-01-29 15:31:53 +0900

    mod : separate component

---
+++ js/953.5a93c6e8.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpp"]=self["webpackChunkpp"]||[]).push([[953],{5953:function(n,e,r){r.r(e),r.d(e,{default:function(){return f}});var u=r(3396);function a(n,e,r,a,t,p){return(0,u.wg)(),(0,u.iD)("h1",null,"main page")}var t={},p=r(89);const c=(0,p.Z)(t,[["render",a],["__scopeId","data-v-68f50247"]]);var f=c}}]);
+//# sourceMappingURL=953.5a93c6e8.js.map
